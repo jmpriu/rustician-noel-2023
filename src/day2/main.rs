465f4bb935617884cefcbd53128159856e815c5f
JM <wolf@pop-os.localdomain>
2023-12-04 22:35:06 +0100

    Add day 2 second part

--- src/day2/main.rs
+++ src/day2/main.rs
@@ -6,6 +6,7 @@ const TOTAL_BLUES: i32 = 14;
 fn main() {
     let stdin = stdin();
     let mut sum = 0;
+    let mut sum_power = 0;
     for games in stdin.lines().map(|l| l.unwrap()) {
         let (game_id, game_results) = games.split_once(": ").unwrap();
         let (_, game_id) = game_id.split_once(" ").unwrap();
@@ -26,9 +27,11 @@ fn main() {
 
             }
         }
+        println!("{game_id}: {}", blues * reds * green);
+        sum_power += blues * reds * green;
         if blues <= TOTAL_BLUES && reds <= TOTAL_REDS && green <= TOTAL_GREEN {
             sum += game_id;
         }
     }
-    println!("{}", sum);
+    println!("Sum: {sum}; Power: {sum_power}");
 }
